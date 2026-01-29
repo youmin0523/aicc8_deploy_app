8389ef6ad20c0c7f53bafa87f4a2103e3104c6ee
youminsu0523 <youminsu0523@gmail.com>
2026-01-29 17:06:03 +0900

    first commit

---
+++ back/index.js
@@ -0,0 +1,21 @@
+// 1. express
+// 2. cors
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config(); // .env 변수 설정
+
+const app = express();
+
+app.use(cors()); // 크로스 오리진 설정
+app.use(express.json()); // json 데이터 파싱
+
+// 3. root 설정
+app.get('/', (request, response) => {
+  response.send('This is the Main App for Deployment');
+});
+
+// 4. listen 설정
+app.listen(process.env.PORT, () => {
+  console.log(`Server is Running on port. ${process.env.PORT}`);
+});
---
+++ front/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline">Hello React !! </h2>
+    </>
+  );
+};
+
+export default App;
