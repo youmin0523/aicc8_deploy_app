962fb77fbd5159277d2b8b461242b5afd3f48302
youminsu0523 <youminsu0523@gmail.com>
2026-01-29 18:13:35 +0900

    backend get task

--- back/index.js
+++ back/index.js
@@ -15,6 +15,8 @@ app.get('/', (request, response) => {
   response.send('This is the Main App for Deployment');
 });
 
+app.use(require('./routes/getRoutes'));
+
 // 4. listen 설정
 app.listen(process.env.PORT, () => {
   console.log(`Server is Running on port. ${process.env.PORT}`);
